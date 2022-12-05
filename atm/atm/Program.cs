b1d304aa7991b7c90622e2f9c0206b5e9ebe7afe
IZUEGBUNAM PERPETUA AMARACHI <54138835+Amarachi32@users.noreply.github.com>
2022-12-05 18:02:21 +0100

    Add files via upload

---
+++ atm/atm/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace atm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var customer1 = new EsteemedCustomer();
+            customer1.CutomerDetails();
+            
+            //entryPoint();
+            //SelectLanguage();
+            
+        }
+
+
+        
+    }
+}
